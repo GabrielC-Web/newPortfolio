ca039877d1af027663c0afd9a9a8b40dfdae6cd2
gcarreno <gcarreno@pagochinchin.com>
2023-03-12 18:49:51 -0400

    Primer prototipo de carousel de proyectos

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import Main from './components/layout/main/main';
 import Navbar from './components/layout/navbar/navbar';
 import Projects from './components/proyectos/proyectos'
-import RouterRenderer from './components/layout/routerRenderer/routerRenderer';
+// import RouterRenderer from './components/layout/routerRenderer/routerRenderer';
+import {useState} from 'react';
 
 let navbarItems = [
   {
@@ -21,49 +22,36 @@ let navbarItems = [
 
 
 function App() {
-
-  /**Detecta el evento de scroll con mouse */
-  function detectScroll(event){
-    var e = event
-    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-
-    // changeRouterView(delta)
-  }
-
-  let spinner = false
-
-  /**Navega hasta la ruta dependiendo del evento scroll */
-  // function changeRouterView(delta){
-  //   for (let i = 0; i < paths.length; i++) {
-  //     const element = paths[i];
-  //     if(window.location.pathname != element){
-  //       if(delta == -1){
-  //         window.location.href = paths[paths.indexOf(element) + 1] || paths[0] 
-  //       } else if(delta == 1){
-  //         window.location.href = paths[paths.indexOf(element) - 1] || paths[paths.length - 1]
-  //       }
-  //     }
-      
+  // const [amountofMovement, setAmountofMovement] = useState(0);
+  // const [count, setCount] = useState(0);
+  // /**Detecta el evento de scroll con mouse */
+  // function detectScroll(event){
+  //   if(event.wheelDeltaY == 0){
+  //     return
   //   }
-  // }
+  //   console.log(event);
+  //   if(event.wheelDeltaY < 0 ){
+  //     setCount( count + 0.5)
+  //   } else{
+  //     setCount( count - 0.5)
+  //   }
 
-  // function handleSpinner(){
-  //   console.log('change!');
+  //   if(count <= 100 && count >= 0){
+  //     setAmountofMovement(String((-count)) + 'vw') 
+  //   }
     
   // }
-
-  // let paths = ['/', 'about', 'projects']
-
-  // window.addEventListener('mousewheel', detectScroll);
-
-  // window.addEventListener('locationchange', handleSpinner)
+  
+  // window.addEventListener('wheel', detectScroll);
 
   return (
-    <div className='container'>
+    <div className='' >
       {/* <RouterRenderer></RouterRenderer> */}
       <Navbar key={navbarItems} items={navbarItems}></Navbar>
-      <Main></Main>
-      <Projects></Projects>
+      <div className='lateralLayout'>
+        <Main></Main>
+        <Projects></Projects>
+      </div>
     </div>
   );
 }
