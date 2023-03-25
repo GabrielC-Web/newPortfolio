deb4dd177fbd51e7c4f7e546dbd88273f624e4bd
gcarreno <gcarreno@pagochinchin.com>
2023-03-25 11:18:29 -0400

    Instalación de FontAwesome

--- src/components/layout/main/main.jsx
+++ src/components/layout/main/main.jsx
@@ -1,7 +1,7 @@
 const Main = (props) => {
     return(
         <div id="main" className="main container">
-            <h1 className="main-title">Bienvenidos al portafolios de Gabriel</h1>
+            <h1 className="main-title">Gabriel Carreño <br /> Desarrollador Frontend</h1>
         </div>
     )
 }
